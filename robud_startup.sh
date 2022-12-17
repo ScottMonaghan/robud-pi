96034523390c7f44911b618d7eaf81072a0dca81
Scott Monaghan <scott.monaghan@gmail.com>
2022-12-17 15:17:57 -0600

    Update robud_startup.sh

--- robud_startup.sh
+++ robud_startup.sh
@@ -4,7 +4,7 @@ export LD_LIBRARY_PATH=/usr/lib/
 export ESPEAK_DATA_PATH=/usr/share/espeak-ng-data
 
 python3 "/home/robud/robud/robud_face/robud_face.py" &
-sleep 15
-python3 "/home/robud/robud/robud_voice/robud_voice.py" &
+python3 "/home/robud/robud/robud_audio/robud_audio.py" &
+
 
 
